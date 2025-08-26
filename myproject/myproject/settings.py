037930673e28dfdca0ed9d880394bb35e641c123
VikasAngadi1 <angadivvikas01@gmail.com>
2025-08-26 14:33:31 +0530

    Some changes in urls.py and settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,3 +120,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
